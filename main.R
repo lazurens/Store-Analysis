d613b614f19f8ed4efd40bdb56d48063adf63773
lazurens <djo_med1990@hotmail.fr>
2017-03-30 14:55:40 +0100

    Increasing sales Hypothesis Draft

--- main.R
+++ main.R
@@ -1,3 +1,8 @@
+# Data Exploration – looking at categorical and continuous feature summaries and making inferences about the data.
+# Data Cleaning – imputing missing values in the data and checking for outliers
+# Feature Engineering – modifying existing variables and creating new ones for analysis
+# Model Building – making predictive models on the data
+
 library(tidyr)
 library(dplyr)
 library(tibble)
@@ -9,3 +14,8 @@ store <- read.csv(file = "data/store.csv")
 # Transformation and Exploration
 add_column(train, from="train")
 data <- bind_rows(train,test)
+
+# Data Exploration 
+# Data Cleaning 
+# Feature Engineering 
+# Model Building 
